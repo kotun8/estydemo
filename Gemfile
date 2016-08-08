6e8843a172adc69deb071bfd63249854bcba5a56
player99 <mkotun@gmail.com>
2016-08-08 20:21:20 +0000

    adding images

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'turbolinks'
 
 gem 'jbuilder', '~> 2.0'
 
+gem "paperclip", "~> 5.0.0"
+
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,7 @@
 class Listing < ActiveRecord::Base
-end
+  has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, :default_url => "default_image.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  end  
+ 
+
+
