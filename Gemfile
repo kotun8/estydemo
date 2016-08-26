6941fe47e4960fee56a1931912a2993c0ef0637a
player99 <mkotun@gmail.com>
2016-08-26 17:53:44 +0000

    Add name to users

--- Gemfile
+++ Gemfile
@@ -17,7 +17,10 @@ gem 'jbuilder', '~> 2.0'
 
 gem "paperclip", "~> 5.0.0"
 
+gem 'devise'
+
 gem 'sdoc', '~> 0.4.0', group: :doc
+
 
 group :production do
   gem 'pg'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #required for devise remember to change local host:3000 to local application host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
